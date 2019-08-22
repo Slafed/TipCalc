07e0bca2dc5d2947aa63572093918993ba175add
slafe <slafed.dixit01@gmail.com>
2019-08-22 13:36:04 -0500

    higdh

---
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.tipcalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    double sahjk = 34.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View v) {
+        DecimalFormat df = new DecimalFormat("#,###,##0.00");
+
+        // Get references to all elements on app screen
+        EditText bill = (EditText) findViewById(R.id.billEditView);
+        EditText tip = (EditText) findViewById(R.id.tipEditView);
+        TextView result = (TextView) findViewById(R.id.resultTextView);
+
+        // get the values from the EditText boxes and convert them to int data types
+        double num1 = Double.parseDouble(bill.getText().toString());
+        double num2 = Double.parseDouble(tip.getText().toString());
+        num2 = num2/100;
+
+        // multiply both ints and format
+       double product = num1*num2;
+
+        // set the text for the answer
+        result.setText("Tip: " + df.format(product));
+    }
+}
